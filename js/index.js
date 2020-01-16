3c16db60064f8f5e4953e1f0db08f22ee4bad915
lpizarro2391 <lourdespizarro@gmail.com>
2020-01-15 21:38:38 -0600

    documento base

---
+++ js/index.js
@@ -0,0 +1,17 @@
+
+document.getElementById('boton-receta1').addEventListener('click', function(){
+
+
+
+
+
+})
+
+document.getElementById('boton-receta2').addEventListener('click', function(){
+
+
+
+
+
+
+})
